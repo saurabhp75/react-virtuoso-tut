4b3689d1a4ef8873c5d89ab6f935ec2b4faf0860
Saurabh Prakash <saurabhp75@gmail.com>
2024-09-14 01:08:42 +0530

    Add feature: Scroll to random item in the list

--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,6 +1,7 @@
 import { type MetaFunction } from '@remix-run/node'
-import { useState } from 'react'
-import { Virtuoso } from 'react-virtuoso'
+import { useRef, useState } from 'react'
+import { Virtuoso, type VirtuosoHandle } from 'react-virtuoso'
+import { Button } from '#app/components/ui/button.js'
 import UserCard from '#app/components/UserCard.js'
 import { createUsers } from './user'
 
@@ -8,10 +9,24 @@ export const meta: MetaFunction = () => [{ title: 'React virtuoso tutorial' }]
 
 export default function Index() {
 	const [users, setUsers] = useState(createUsers)
+	const virtuosoRef = useRef<VirtuosoHandle>(null)
 	return (
 		// grid and flex classnames not working with Virtuoso
 		<main>
+			<Button
+				className="mb-4"
+				onClick={() => {
+					virtuosoRef.current?.scrollToIndex({
+						index: Math.random() * users.length,
+						align: 'start',
+						behavior: 'smooth',
+					})
+				}}
+			>
+				Scroll
+			</Button>
 			<Virtuoso
+				ref={virtuosoRef}
 				className="!h-[300px]"
 				data={users}
 				itemContent={(_, user) => <UserCard user={user} />}
