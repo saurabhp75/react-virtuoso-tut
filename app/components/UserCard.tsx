d9bee049efb1a961dfbf54576ac42414d899a92a
Saurabh Prakash <saurabhp75@gmail.com>
2024-09-14 02:52:40 +0530

    Convert list to a table

--- app/components/UserCard.tsx
+++ app/components/UserCard.tsx
@@ -3,8 +3,8 @@ import { type User } from '#app/routes/_marketing+/user.js'
 export default function UserCard({ user }: { user: User }) {
 	return (
 		<>
-			<div>{user.id}</div>
-			<div>{user.name}</div>
+			<td>{user.id}</td>
+			<td>{user.name}</td>
 		</>
 	)
 }
--- app/routes/_marketing+/index.tsx
+++ app/routes/_marketing+/index.tsx
@@ -1,6 +1,6 @@
 import { type MetaFunction } from '@remix-run/node'
 import { useRef, useState } from 'react'
-import { Virtuoso, type VirtuosoHandle } from 'react-virtuoso'
+import { TableVirtuoso, type VirtuosoHandle } from 'react-virtuoso'
 import { Button } from '#app/components/ui/button.js'
 import UserCard from '#app/components/UserCard.js'
 import { createUsers } from './user'
@@ -9,7 +9,20 @@ export const meta: MetaFunction = () => [{ title: 'React virtuoso tutorial' }]
 
 export default function Index() {
 	const [users, setUsers] = useState(createUsers)
+	const [isLoading, setIsLoading] = useState(false)
 	const virtuosoRef = useRef<VirtuosoHandle>(null)
+
+	async function fetchNextPage() {
+		const newUsers = createUsers(users.length, users.length + 20)
+
+		setIsLoading(true)
+		await new Promise((resolve) => setTimeout(resolve, 1000))
+		setIsLoading(false)
+
+		setUsers([...users, ...newUsers])
+	}
+
+
 	return (
 		// grid and flex classnames not working with Virtuoso
 		<main>
@@ -25,11 +38,23 @@ export default function Index() {
 			>
 				Scroll
 			</Button>
-			<Virtuoso
+			<TableVirtuoso
 				ref={virtuosoRef}
 				className="!h-[300px]"
 				data={users}
+				endReached={fetchNextPage}
 				itemContent={(_, user) => <UserCard user={user} />}
+				fixedFooterContent={
+					isLoading
+						? () => <div className="bg-grayscale-700">Loading...</div>
+						: undefined
+				}
+				fixedHeaderContent={() => (
+					<tr className='bg-background'>
+						<th className="bg-grayscale-700 w-[150px] text-left">Id</th>
+						<th className="bg-grayscale-700 w-[150px] text-left">Name</th>
+					</tr>
+				)}
 			/>
 		</main>
 	)
